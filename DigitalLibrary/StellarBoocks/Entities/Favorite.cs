c79e276fcae4989880064c4f15c00f70c7b1beee
WandrysFG <wandrysf@gmail.com>
2025-07-19 21:09:58 -0400

    Creating entities and installing Entities Framework packages

---
+++ DigitalLibrary/StellarBoocks/Entities/Favorite.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace StellarBoocks.Entities
+{
+    [Table("Favorites")]
+    public class Favorite
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [Required]
+        public int TaleId { get; set; }
+
+        [ForeignKey("TaleId")]
+        public Tale Tale { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DateAdded { get; set; } = DateTime.Today;
+    }
+}
---
+++ DigitalLibrary/StellarBoocks/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace StellarBoocks.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required, StringLength(100), EmailAddress]
+        public string Email { get; set; }
+
+        [Required, StringLength(255)]
+        public string Password { get; set; }
+
+        [Required, StringLength(20)]
+        public string UserType { get; set; } = "Reader";
+
+        [Column(TypeName = "date")]
+        public DateTime RegistrationDate { get; set; } = DateTime.Today;
+
+        public bool IsActive { get; set; } = true;
+
+        public ICollection<Favorite> Favorites { get; set; }
+    }
+
+}
